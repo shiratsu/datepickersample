aa1f1e677936120abb74b93176536c9a439b7822
平塚俊輔 <shun_hiratsuka2002@yahoo.co.jp>
2015-06-11 02:27:34 +0900

    出来たのでコミット

--- datepickersample/ViewController.swift
+++ datepickersample/ViewController.swift
@@ -10,9 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var first_text: UITextField!
+    @IBOutlet weak var second_text: UITextField!
+    
+    
+    lazy var dateFormatter: NSDateFormatter = {
+        let dateFormatter = NSDateFormatter()
+        
+        
+        dateFormatter.dateFormat = "HH:mm"
+        
+        return dateFormatter
+        }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let datepicker = UIDatePicker()
+        datepicker.datePickerMode = UIDatePickerMode.Time
+        first_text.inputView = datepicker
+        
+        datepicker.addTarget(self, action: "updateFirst:", forControlEvents: UIControlEvents.ValueChanged)
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +40,13 @@ class ViewController: UIViewController {
     }
 
 
+    func updateFirst(sender:AnyObject?){
+        var dateSelecter: UIDatePicker = sender as! UIDatePicker
+        let datestr = dateFormatter.stringFromDate(dateSelecter.date);
+        println(datestr);//Oct 23, 2014, 7:58 PMな風に出力されます。
+        first_text.text = datestr
+    }
+
+    
 }
 
